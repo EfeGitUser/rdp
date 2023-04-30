58fa70602b4ee9c99edc51668e21b83a70bf1cd0
EfeeMethod <131386664+EfeeMethod@users.noreply.github.com>
2023-04-30 22:46:14 +0300

    Update enable-access.ps1

--- enable-access.ps1
+++ enable-access.ps1
@@ -1,2 +1,5 @@
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+net.exe user $env:UserName "Password@001"
+write-host Username: $env:UserName
+write-host Password: Password@001
