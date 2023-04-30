97b0d9945938c3fdc9be1274648d6c8b0cfe4871
EfeeMethod <131386664+EfeeMethod@users.noreply.github.com>
2023-04-30 22:11:18 +0300

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,10 @@
+echo Adding User And Admin Access..
+net user rdp_user rdpuser@001 /add
+net localgroup administrators rdp /add
+echo Enabling Remote Access..
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+echo Adding Token
+.\ngrok.exe config add-authtoken 2OhI9NwNaO3Ky1yJKPbYR5Nq710_69Cs72CWeYoHmWKAzhQ9w
+echo Opening TCP..
+.\ngrok.exe tcp 3389
