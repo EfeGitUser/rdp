ffeea30b9ec64422ede84d2b337c7f3f1f4a24fe
EfeeMethod <131386664+EfeeMethod@users.noreply.github.com>
2023-04-30 22:19:25 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -6,5 +6,8 @@ Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 echo Adding Token
 .\ngrok.exe config add-authtoken 2OhI9NwNaO3Ky1yJKPbYR5Nq710_69Cs72CWeYoHmWKAzhQ9w
+echo RDP Username And Pass:
+echo Username: rdp_user
+echo Pass: rdpuser@001
 echo Opening TCP..
 .\ngrok.exe tcp 3389
